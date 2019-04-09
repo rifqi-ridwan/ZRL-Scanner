df21d320852bca48051f65ed36d9cb7a75830adb
Muhammad Rifqi <muhammad.rifqi@soltius.co.id>
2019-04-09 07:02:32 +0000

    update lagi

--- controller/View1.controller.js
+++ controller/View1.controller.js
@@ -1,12 +1,7 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-<<<<<<< HEAD
 	"sap/ndc/BarcodeScanner",
 	"sap/m/MessageBox"
-=======
-	"sap/m/MessageBox",
-	"sap/ndc/BarcodeScanner"
->>>>>>> refs/remotes/origin/master
 ], function (Controller, BarcodeScanner,MessageBox) {
 	"use strict";
 
@@ -18,7 +13,7 @@ sap.ui.define([
 		onPress: function () {
 			var currView = this.getView();
 			var test = 'check';
-			BarcodeScanner.scan(
+			sap.ndc.BarcodeScanner.scan(
 				function (mResult) {
 					var textbox = currView.byId('inpt_valuescn');
 					textbox.setValue(mResult.text);
